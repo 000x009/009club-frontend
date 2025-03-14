3d3f7e36d915e210cde5c56e7f5addb8502ad822
yuapX <shesterovmm@gmail.com>
2025-03-14 18:33:09 +0200

    fix paypal attempt 3

--- src/app/const/paypalConfig.js
+++ src/app/const/paypalConfig.js
@@ -1,18 +1,7 @@
 import { appSettings } from "@/app/const/settings.js";
-import { getUserLanguage } from "@/shared/lib/helpers/getUserLanguage.js";
-
-const locale = getUserLanguage();
-
-const languagePaypal = () => {
-    return locale.language + "_" + locale.language.toUpperCase();
-}
 
 export const paypalOptions = {
-    "client-id": appSettings.PAYPAL_CLIENT_ID,
+    clientId: appSettings.PAYPAL_CLIENT_ID,
     currency: "EUR",
-    locale: languagePaypal(),
     components: "buttons",
-    "buyer-country": "BG",
-    debug: true,
-    "data-page-type": "checkout",
 };
